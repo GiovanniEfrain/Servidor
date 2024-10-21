3582cafc7d3b3632428b36678c6015549b83197f
GiovanniEfrain <efrain98vc@gmail.com>
2024-10-21 15:03:51 -0600

    Servidor con mensaje

--- ServidorAtl/src/index.ts
+++ ServidorAtl/src/index.ts
@@ -1,16 +1,20 @@
-import express, { Request, Response } from 'express'; 
+import express, { Application, json, Request, Response } from 'express'; 
+import cors from "cors";
 
+// Constants 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+//app.use(cors());
 app.use(express.json()); 
 
-
+// Get 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Wolrd');
-})
+    res.send('API RUNNIG...');
+});
 
-app.listen(port, () => {
+app.listen(port, () => { 
     console.log(
-        'Server running at http://localhost$'
+       `Server running at http://localhost:${port}/`
     )
-})
+});
